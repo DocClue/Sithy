7c8957ed8ddcc48091c345662b18820e6aa8c1cf
Dr.Clue <twilight71777@gmail.com>
2015-11-06 14:44:23 -0700

    Initial Commit

---
+++ Sithy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sithy
+//
+//  Created by Timothy Hartt on 11/6/15.
+//  Copyright (c) 2015 The Sith Lords. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
